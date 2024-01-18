a93863b5b5a5e28d00c3fcb416aa36c239ea39b5
qq568986318 <568986318@qq.com>
2024-01-18 17:43:23 +0800

    代码都已经完毕 差 部署项目  先提交代码

--- spzx-parent/spzx-service/service-user/src/main/java/com/atguigu/spzx/user/UserApplication.java
+++ spzx-parent/spzx-service/service-user/src/main/java/com/atguigu/spzx/user/UserApplication.java
@@ -1,7 +1,6 @@
 package com.atguigu.spzx.user;
 
 import com.atguigu.spzx.common.anno.EnableUserLoginAuthInterceptor;
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
@@ -11,7 +10,7 @@ import org.springframework.context.annotation.ComponentScan;
  */
 @SpringBootApplication
 @EnableUserLoginAuthInterceptor
-@ComponentScan(basePackages = "com.atguigu.spzx")
+//@ComponentScan(basePackages = "com.atguigu.spzx") //启用这个 swger 失效这个是后台用的
 public class UserApplication {
     public static void main(String[] args) {
         SpringApplication.run(UserApplication.class,args);
